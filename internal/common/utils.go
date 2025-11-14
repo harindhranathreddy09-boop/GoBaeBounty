9f25e966de85396d2fafb48832cbe6b3a685d9fb
HarryR <harindhranathreddy09@gmail.com>
2025-11-15 01:59:15 +0530

    Update utils.go

--- internal/common/utils.go
+++ internal/common/utils.go
@@ -1,52 +1,49 @@
 package common
 
 import (
-	"net/url"
-	"regexp"
-	"strings"
+    "net/url"
+    "regexp"
+    "strings"
 )
-
-// NormalizeURL removes fragments and standardizes URL for deduplication
-func NormalizeURL(rawURL string) (string, error) {
-	u, err := url.Parse(rawURL)
-	if err != nil {
-		return "", err
-	}
-	u.Fragment = ""
-	u.Host = strings.ToLower(u.Host)
-	return u.String(), nil
-}
-
-// ExtractDomain extracts domain part of URL
-func ExtractDomain(rawURL string) string {
-	u, err := url.Parse(rawURL)
-	if err != nil {
-		return ""
-	}
-	return u.Hostname()
-}
-
-// IsInScope checks if URL domain belongs to scope domain
-func IsInScope(targetURL, scopeDomain string) bool {
-	domain := ExtractDomain(targetURL)
-	return domain == scopeDomain || strings.HasSuffix(domain, "."+scopeDomain)
-}
 
 // Deduplicate removes duplicate strings from slice
 func Deduplicate(items []string) []string {
-	seen := make(map[string]bool)
-	var result []string
-	for _, s := range items {
-		if !seen[s] {
-			result = append(result, s)
-			seen[s] = true
-		}
-	}
-	return result
+    seen := make(map[string]bool)
+    var result []string
+    for _, s := range items {
+        s = strings.TrimSpace(s)
+        if s != "" && !seen[s] {
+            seen[s] = true
+            result = append(result, s)
+        }
+    }
+    return result
 }
 
-// IsJavaScriptFile returns true if URL path looks like a JS file
-func IsJavaScriptFile(rawURL string) bool {
-	re := regexp.MustCompile(`(?i)\.js(\?.*)?$`)
-	return re.MatchString(rawURL)
+// NormalizeURL removes fragments and standardizes URL for deduplication
+func NormalizeURL(rawURL string) (string, error) {
+    u, err := url.Parse(rawURL)
+    if err != nil {
+        return "", err
+    }
+    u.Fragment = ""
+    u.Host = strings.ToLower(u.Host)
+    return u.String(), nil
 }
+
+// ExtractDomain gets the hostname from a URL
+func ExtractDomain(rawURL string) string {
+    u, err := url.Parse(rawURL)
+    if err != nil {
+        return ""
+    }
+    return u.Hostname()
+}
+
+// IsJavaScriptFile returns true if it looks like a JS file/link
+func IsJavaScriptFile(rawURL string) bool {
+    re := regexp.MustCompile(`(?i)\.js(\?|$)`)
+    return re.MatchString(rawURL)
+}
+
+// Add any remaining utility functions here...
