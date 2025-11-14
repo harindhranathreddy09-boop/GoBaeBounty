516b926cbb58467d95f126dbcca500a55969f539
HarryR <harindhranathreddy09@gmail.com>
2025-11-15 01:09:30 +0530

    Update utils.go

--- internal/common/utils.go
+++ internal/common/utils.go
@@ -1,8 +1,6 @@
 package common
 
 import (
-	"crypto/md5"
-	"encoding/hex"
 	"net/url"
 	"regexp"
 	"strings"
@@ -35,13 +33,65 @@ func IsInScope(targetURL, scopeDomain string) bool {
 }
 
 // Deduplicate removes duplicate strings from slice
-func Deduplicate(strings []string) []string {
+func Deduplicate(items []string) []string {
 	seen := make(map[string]bool)
 	var result []string
-	for _, s := range strings {
-		if _, exists := seen[s]; !exists {
-			seen[s] = true
+	for _, s := range items {
+		if !seen[s] {
 			result = append(result, s)
+			seen[s] = true
+		}
+	}
+	return result
+}
+
+// IsJavaScriptFile returns true if URL path looks like a JS file
+func IsJavaScriptFile(rawURL string) bool {
+	re := regexp.MustCompile(`(?i)\.js(\?.*)?$`)
+	return re.MatchString(rawURL)
+}
+package common
+
+import (
+	"net/url"
+	"regexp"
+	"strings"
+)
+
+// NormalizeURL removes fragments and standardizes URL for deduplication
+func NormalizeURL(rawURL string) (string, error) {
+	u, err := url.Parse(rawURL)
+	if err != nil {
+		return "", err
+	}
+	u.Fragment = ""
+	u.Host = strings.ToLower(u.Host)
+	return u.String(), nil
+}
+
+// ExtractDomain extracts domain part of URL
+func ExtractDomain(rawURL string) string {
+	u, err := url.Parse(rawURL)
+	if err != nil {
+		return ""
+	}
+	return u.Hostname()
+}
+
+// IsInScope checks if URL domain belongs to scope domain
+func IsInScope(targetURL, scopeDomain string) bool {
+	domain := ExtractDomain(targetURL)
+	return domain == scopeDomain || strings.HasSuffix(domain, "."+scopeDomain)
+}
+
+// Deduplicate removes duplicate strings from slice
+func Deduplicate(items []string) []string {
+	seen := make(map[string]bool)
+	var result []string
+	for _, s := range items {
+		if !seen[s] {
+			result = append(result, s)
+			seen[s] = true
 		}
 	}
 	return result
